efc5840b368954bce9539a1e7198fb47ff2dcc9a
janwe <janwe@LAPTOP-FLN6CH61>
2022-10-06 16:44:13 +0800

    Add project files.

---
+++ Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RenielDavid.RazorTestWebApp.Data;
+using RenielDavid.RazorTestWebApp.Models;
+
+namespace RenielDavid.RazorTestWebApp.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly RenielDavid.RazorTestWebApp.Data.RenielDavidRazorTestWebAppContext _context;
+
+        public DetailsModel(RenielDavid.RazorTestWebApp.Data.RenielDavidRazorTestWebAppContext context)
+        {
+            _context = context;
+        }
+
+      public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+
+
+            if (id == null || _context.Movie == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Movie = movie;
+            }
+            return Page();
+        }
+    }
+}
